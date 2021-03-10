db385b384032f2b29ecd72991d011f2369f4a27e
David CARLIER <devnexen@gmail.com>
2021-03-10 09:04:01 -0800

    Mac M1 crc32 intrinsics ARM64 check support proposal (#7893)
    Summary: Pull Request resolved: https://github.com/facebook/rocksdb/pull/7893
    
    Reviewed By: ajkr
    
    Differential Revision: D26050966
    
    Pulled By: jay-zhuang
    
    fbshipit-source-id: 9df2bb65d82defd7fad49d5369979b03e22d39c2

--- util/crc32c_arm64.cc
+++ util/crc32c_arm64.cc
@@ -19,6 +19,9 @@
 #ifndef HWCAP_PMULL
 #define HWCAP_PMULL (1 << 4)
 #endif
+#if defined(__APPLE__)
+#include <sys/sysctl.h>
+#endif
 
 #ifdef HAVE_ARM64_CRYPTO
 /* unfolding to compute 8 * 3 = 24 bytes parallelly */
@@ -43,20 +46,33 @@
 extern bool pmull_runtime_flag;
 
 uint32_t crc32c_runtime_check(void) {
-#ifdef ROCKSDB_AUXV_GETAUXVAL_PRESENT
-  uint64_t auxv = getauxval(AT_HWCAP);
+#if !defined(__APPLE__)
+  uint64_t auxv = 0;
+#if defined(ROCKSDB_AUXV_GETAUXVAL_PRESENT)
+  auxv = getauxval(AT_HWCAP);
+#elif defined(__FreeBSD__)
+  elf_aux_info(AT_HWCAP, &auxv, sizeof(auxv));
+#endif
   return (auxv & HWCAP_CRC32) != 0;
 #else
-  return 0;
+  int r;
+  size_t l = sizeof(r);
+  if (sysctlbyname("hw.optional.armv8_crc32", &r, &l, NULL, 0) == -1) return 0;
+  return r == 1;
 #endif
 }
 
 bool crc32c_pmull_runtime_check(void) {
-#ifdef ROCKSDB_AUXV_GETAUXVAL_PRESENT
-  uint64_t auxv = getauxval(AT_HWCAP);
+#if !defined(__APPLE__)
+  uint64_t auxv = 0;
+#if defined(ROCKSDB_AUXV_GETAUXVAL_PRESENT)
+  auxv = getauxval(AT_HWCAP);
+#elif defined(__FreeBSD__)
+  elf_aux_info(AT_HWCAP, &auxv, sizeof(auxv));
+#endif
   return (auxv & HWCAP_PMULL) != 0;
 #else
-  return false;
+  return true;
 #endif
 }
 
